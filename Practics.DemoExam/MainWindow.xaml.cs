8400c38d47d76b3b8150500db70afb39454485b3
Олег Абрамов <rufus116rus@gmail.com>
2022-11-05 08:49:55 +0300

    - Init

---
+++ Practics.DemoExam/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Practics.DemoExam
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
